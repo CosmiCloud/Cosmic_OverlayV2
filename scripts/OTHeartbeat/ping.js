ba901b9dfa717e923bffbf3739c73be7fc2b7604
CosmiCloud <tybranha@gmail.com>
2021-11-08 10:13:41 -0500

    added bid check to ping script to restart if node is not bidding

--- scripts/OTHeartbeat/ping.js
+++ scripts/OTHeartbeat/ping.js
@@ -39,7 +39,7 @@ module.exports={
 
 async function ping(){
   try{
-    console.log(date+' - scripts/ping.js: Pinging...');
+    console.log(date+' - scripts/ping.js: Pinging otnode...');
     var runStateq = "sudo docker inspect -f {{.State.Running}} otnode"
     var running = await exec(runStateq);
     var running = running.stdout
@@ -48,38 +48,47 @@ async function ping(){
     if(running == 'true'){
       console.log(date+' - scripts/ping.js: otnode is running.');
 
-      // console.log(date+' - scripts/ping.js: Checking for bids...');
-      // var bid_check = "sudo docker logs --since 1h otnode | grep 'Accepting offer'"
-      // var bid_check = await exec(bid_check);
-      //
-      // console.log(bid_check)
-      // const jobs = await module.exports.job_check();
-      // console.log(jobs)
-      //
-      // if(bid_check = undefined && jobs !== '0'){
-      //   await client.sendMessage(chatId, node_name+' hasnt bid on a job in the last hour and there have been jobs. Restarting...', {
-      //     disableWebPagePreview: true,
-      //     disableNotification: false,
-      //   });
-      //
-      // }else{
-        console.log(date+' - scripts/ping.js: Checking CPU usage...');
-         var checkcpu = "echo $(vmstat 1 2|tail -1|awk '{print $15}')"
-         var checkcpu = await exec(checkcpu);
-         var cpuusage = checkcpu.stdout
-         var cpuusage = Number(cpuusage);
-         var cpuusage = 100 - cpuusage
+      console.log(date+' - scripts/ping.js: checking if node has bid recently');
+      var bid_check = "sudo docker logs --since 1h otnode | grep 'Accepting' | wc -l"
+      var bid_check = await exec(bid_check);
+      var bids = bid_check.stdout
+      var bids = Number(bids);
+      console.log(date+' - scripts/ping.js: bids -> '+bids)
 
+      console.log(date+' - scripts/ping.js: checking if there have been jobs recently');
+      var jobs_count = 'sudo curl -sX GET "https://v5api.othub.info/api/jobs/jobcreatedcountinperiod?timePeriod=hours&time=1&blockchainID=2"'
+      var jobs_count = await exec(jobs_count);
+      var jobs_count = jobs_count.stdout
+      var jobs_count = Number(jobs_count);
+      console.log(date+' - scripts/ping.js: job count -> '+jobs_count)
+
+      console.log(date+' - scripts/ping.js: Checking CPU usage...');
+       var checkcpu = "echo $(vmstat 1 2|tail -1|awk '{print $15}')"
+       var checkcpu = await exec(checkcpu);
+       var cpuusage = checkcpu.stdout
+       var cpuusage = Number(cpuusage);
+       var cpuusage = 100 - cpuusage
+       console.log(date+' - scripts/ping.js: CPU usage -> '+cpuusage);
+
+       if(bids == 0){
+         if(jobs_count !== 0){
+           console.log(date+' - scripts/ping.js: node has NOT been bidding and there have been jobs. Attempting to restart..');
+           await client.sendMessage(chatId, node_name+ ' has NOT bid in the last hour and there have been jobs. Attempting to restart..', {
+             disableWebPagePreview: true,
+             disableNotification: false,
+           });
+           var restart = "sudo docker start otnode"
+           await exec(restart);
+         }
+       }else{
          if(cpuusage > 80){
-           console.log(date+' - scripts/ping.js: CPU usage is high:'+usage+'%');
-           await client.sendMessage(chatId, node_name+ ' CPU usage is '+ usage+'%', {
+           console.log(date+' - scripts/ping.js: CPU usage is high:'+cpuusage+'%');
+           await client.sendMessage(chatId, node_name+ ' CPU usage is '+ cpuusage+'%', {
              disableWebPagePreview: true,
              disableNotification: false,
            });
          }
-      //}
-
-      return;
+       }
     }else if(running == 'false'){
         console.log(date+' - scripts/ping.js: otnode is NOT running.');
         await client.sendMessage(chatId, node_name+ ' is not running. Attempting to restart..', {
